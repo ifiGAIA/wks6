fd27a22b334511baa699065695226f9d1b75660c
ifigaia <wet159@gmail.com>
2019-04-08 19:37:13 +0800

    css  commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 let path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry:'./assets/js/script.js',
     output:{
         path:path.join(__dirname,'./dist'),
         filename:'bundle.js',
-        // publicPath:'./dist/'
+        //publicPath:'./dist/'
     },
     module:{
         rules:[
-            
+            {
+                use: ExtractTextPlugin.extract({
+                  use: "css-loader"
+                }),
+                test: /\.css$/,
+              },
         {
             test:/\.(jpe?g|png|gif|svg)$/,
             use:[
@@ -17,13 +23,19 @@ module.exports = {
                 loader:'url-loader',
                 options:{
                     limit:400000,
-                    outputPath:'./images'
+                    outputPath:'./images',
+                    publicPath: './images'
                 }
                 },
                 'image-webpack-loader'
         
     ]
 }
-    ]
-    }
-}
+
+]
+},
+plugins: [
+    new ExtractTextPlugin('./css/style.css'),
+    
+  ]
+  }
